164f1390d8cb4a5edd0606fe60f2091e4cde827c
Dev_body <86171864+devbody@users.noreply.github.com>
2022-03-22 19:16:42 +0200

    Add files via upload

---
+++ ch3.js
@@ -0,0 +1,49 @@
+
+ //Built in functions 
+ parselnt()>>>>
+ varx="add33" // parsint = nan
+ x="33.5.dd"     // parsint = 33  first
+ parseFloat()>>>>>>
+ b=66.66.6 =  // 55.66  first point
+ isNaN()>>>>>
+ isNaN(4948) //false
+ isNaN(fjlas) // true
+ isFinite()>>>>>>
+ isFinite(fklaj) // false
+ isFinite(323333) // true
+ decodeURI()>>>
+ encodeURI()>>>>
+
+ //User defined function
+ function addtowparam(x,y){
+   
+     sum= x+y
+     return x+y;
+ }
+ //call
+ var result = addtowparam(5,6)
+ console.log(result);
+
+ //Default value
+ بنده علي الفانكشن
+
+ num= x=4+y=9
+ //Variable scope - Shadowing
+ global scope   // genral 
+ local scope    // 2 التاني
+
+
+ //Shadowing
+
+ var c=4
+  console.log(c)
+ function(){
+     var c= 4
+     console.log(c)
+ }
+
+ //JS Debugging
+ tools>> sources
+call stakc >>>    بتشيل الفانكشن
+ watch   a = كااام 
+ 
